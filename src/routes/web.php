cda88d4cdb12aeef243580638c07b095d97d2c36
yohyama <yohyama0216>
2023-10-09 16:00:47 +0900

    仮でURL作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,47 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\ReviewController;
+use App\Http\Controllers\Admin\SaleController;
+use App\Http\Controllers\Admin\SettingController;
+use App\Http\Controllers\Admin\LogController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::prefix('admin')->group(function () {
+    // ログイン・認証関連
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->name('admin.logout');
 
-Route::get('/', function () {
-    return view('welcome');
+    // 認証が必要なルートはこのミドルウェア内に配置
+    // Route::middleware(['auth:admin'])->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
+
+        // 商品関連
+        Route::resource('/products', ProductController::class);
+
+        // カテゴリ関連
+        Route::resource('/categories', CategoryController::class);
+
+        // 注文関連
+        Route::resource('/orders', OrderController::class);
+
+        // ユーザー・顧客関連
+        Route::resource('/users', UserController::class);
+
+        // レビュー・フィードバック関連
+        Route::resource('/reviews', ReviewController::class);
+
+        // セールス・プロモーション関連
+        Route::resource('/sales', SaleController::class);
+
+        // 設定・その他
+        Route::get('/settings', [SettingController::class, 'index'])->name('admin.settings');
+        Route::get('/logs', [LogController::class, 'index'])->name('admin.logs');
+    // });
 });
